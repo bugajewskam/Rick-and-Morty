951f2bc9099274f9b321129f107696eebfa3f5d6
bugajewskam <53918817+bugajewskam@users.noreply.github.com>
2022-05-12 00:02:21 +0200

    Add favorites functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
-import  CharacterPage  from "./pages/CharacterPage";
-import  CharactersPage  from "./pages/CharactersPage";
+import CharacterPage from "./pages/CharacterPage";
+import CharactersPage from "./pages/CharactersPage";
 
+export const FavouriteContext = React.createContext({ favourites: [] as number[], addFavourite(id: number) { }, removeFavourite(id: number) { } });
 function App() {
+  const [favourites, setFavourites] = useState<number[]>([])
+  const addFavourite = useCallback((id: number) => {
+    setFavourites(prev => [...prev, id])
+  }, [])
+
   return (
-    <div className="App">
-       <Routes>
-        <Route path="characters" element={<CharactersPage />} />
-        <Route path="character/:id" element={<CharacterPage />} />
-      </Routes>
-    </div>
+    <FavouriteContext.Provider value={{ favourites, addFavourite, removeFavourite: (id) => { } }}>
+      <div className="App">
+        <Routes>
+          <Route path="characters" element={<CharactersPage />} />
+          <Route path="character/:id" element={<CharacterPage />} />
+
+
+        </Routes>
+      </div>
+    </FavouriteContext.Provider>
   );
 }
 
--- src/pages/CharactersPage.tsx
+++ src/pages/CharactersPage.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { FavouriteContext } from '../App';
 import { data } from "../data/mock"
 function CharactersPage() {
-
+    const { favourites, addFavourite } = useContext(FavouriteContext)
     return (
         <div >
             Characters Page
             <ul>
-                {data.map(character => 
-                    <Link key={character.id} to={`/character/${character.id}`}><li>{character.name}</li></Link>
+                {data.map(character =>
+                    
+                        <li key={character.id}>
+                            <Link to={`/character/${character.id}`}>{character.name}</Link>
+                            {!favourites.includes(character.id) && <button onClick={() => addFavourite(character.id) }>Add to favourite</button>}
+                        </li>
+            
                 )}
             </ul>
         </div>
