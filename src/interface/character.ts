7e24981f069964fd279d7a720dcfe736c643bb39
bugajewskam <53918817+bugajewskam@users.noreply.github.com>
2022-05-12 01:26:25 +0200

    Add character interface

---
+++ src/interface/character.ts
@@ -0,0 +1,16 @@
+export interface Character {
+    id:       number;
+    name:     string;
+    status:   string;
+    species:  string;
+    type:     string;
+    gender:   string;
+    origin:   Location;
+    location: Location;
+    image:    string;
+    created:  Date;
+}
+
+export interface Location {
+    name: string;
+}
